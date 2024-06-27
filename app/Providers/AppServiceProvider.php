004a6cadfb06f883b47eaaacca02b4347106d64f
Alexandra Smirnova <smir0002@hz.nl>
2024-06-27 18:46:34 +0200

    lalal

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,31 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        // Force HTTPS redirection if not in local environment
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
